4f62b7745e0357e79f8632efb71ac9ccac371b63
Rafał Krupa <rafal.krupa@cetuspro.com>
2024-10-16 18:32:41 +0200

    Move folders

--- PrimeNumbers/Forms/PrimeNumbersForm.cs
+++ PrimeNumbers/Forms/PrimeNumbersForm.cs
@@ -1,37 +1,28 @@
 using PrimeNumbers.DTO;
 using PrimeNumbers.Interfaces;
-using PrimeNumbers.Services;
 
 namespace PrimeNumbers
 {
     internal partial class PrimeNumbersForm : Form
     {
         private ICalculator _calculator;
+        private IResultsSaver _resultsSaver;
         private Thread _thread;
         private CancellationTokenSource _cancellationTokenSource;
         private System.Windows.Forms.Timer _timer;
         private bool _isCalculating = false;
-        private IResultsSaver _resultsSaver;
         private List<CycleResultDto> _cycleResults = new();
 
         public PrimeNumbersForm(ICalculator calculator, IResultsSaver resultsSaver)
         {
             InitializeComponent();
-            _cancellationTokenSource = new CancellationTokenSource();
             _calculator = calculator;
+            _resultsSaver = resultsSaver;
+            _cancellationTokenSource = new CancellationTokenSource();
             _calculator.ErrorOccurred += OnErrorOccurred;
             _calculator.CycleCompleted += OnCycleCompleted;
-            _resultsSaver = resultsSaver;
             SetupListView();
             SetupStatusTimer();
-        }
-
-        private void SetupStatusTimer()
-        {
-            _timer = new System.Windows.Forms.Timer();
-            _timer.Interval = 100;
-            _timer.Tick += UpdateStatusLabel;
-            _timer.Start();
         }
 
         private void UpdateStatusLabel(object sender, EventArgs e)
@@ -44,17 +35,6 @@ namespace PrimeNumbers
             {
                 lblStatus.Text = "Obliczanie nieaktywne";
             }
-        }
-
-        private void SetupListView()
-        {
-            listView.View = View.Details;
-            listView.Columns.Add("Numer cyklu", 100);
-            listView.Columns.Add("Najwiêksza liczba pierwsza", 200);
-            listView.Columns.Add("Czas cyklu", 150);
-            listView.Columns.Add("Godzina zakoñczenia cyklu", 200);
-            listView.FullRowSelect = true;
-            listView.GridLines = true;
         }
 
         private async void btnStart_Click(object sender, EventArgs e)
@@ -135,6 +115,25 @@ namespace PrimeNumbers
             {
                 MessageBox.Show($"B³¹d podczas zapisywania wyników: {ex.Message}", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void SetupListView()
+        {
+            listView.View = View.Details;
+            listView.Columns.Add("Numer cyklu", 100);
+            listView.Columns.Add("Najwiêksza liczba pierwsza", 200);
+            listView.Columns.Add("Czas cyklu", 150);
+            listView.Columns.Add("Godzina zakoñczenia cyklu", 200);
+            listView.FullRowSelect = true;
+            listView.GridLines = true;
+        }
+
+        private void SetupStatusTimer()
+        {
+            _timer = new System.Windows.Forms.Timer();
+            _timer.Interval = 100;
+            _timer.Tick += UpdateStatusLabel;
+            _timer.Start();
         }
     }
 }
